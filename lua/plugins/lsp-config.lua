1885bb80ed4177e1b4ffc272c3c7b312a51b27f5
Evan Mitchell <ejm0214@gmail.com>
2024-05-26 16:58:31 -0400

    Updates

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,6 +22,7 @@ return {
       lspconfig.lua_ls.setup({ capabilities = capabilities })
       lspconfig.html.setup({ capabilities = capabilities })
       lspconfig.tsserver.setup({ capabilities = capabilities })
+      lspconfig.clangd.setup({ capabilities = capabilities })
     end
   },
 }
